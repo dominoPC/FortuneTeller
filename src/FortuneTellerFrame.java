9eda3e602a58f4eeac80896a40f331550580bf76
dominoPC <89866615+dominoPC@users.noreply.github.com>
2023-09-25 09:36:14 -0400

    init

---
+++ src/FortuneTellerFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class FortuneTellerFrame extends JFrame{
+    public FortuneTellerFrame() {
+        setTitle("Fortune Teller");
+        setSize(500, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
