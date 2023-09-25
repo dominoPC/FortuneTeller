7d6fefd64fb831440e00c77d60497706a1bad6a2
dominoPC <89866615+dominoPC@users.noreply.github.com>
2023-09-25 11:42:05 -0400

    added fortunes

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 
 public class FortuneTellerFrame extends JFrame{
     JPanel mainPnl;
@@ -17,7 +18,8 @@ public class FortuneTellerFrame extends JFrame{
     Font sans;
     JButton readBtn;
     JButton quitBtn;
-
+    String[] fortunes = {"You look upside-down through this thing","I forsee a restaurant disaster","You will forget why you entered a room","I see great success in fishing","You will meet your doppelgaenger","30% chance of scattered showers today","Your sense of smell will reward you","ERROR 404: Lucky number not found","You will close this window","You will read this fortune","History will repeat itself","Someone you know will tell you hello"};
+    short i = 12;
     public FortuneTellerFrame() {
 
         mainPnl = new JPanel();
@@ -32,7 +34,7 @@ public class FortuneTellerFrame extends JFrame{
 
 
         serif = new Font(Font.SERIF, Font.PLAIN, 18);
-        textArea = new JTextArea("fortunes",10,40);
+        textArea = new JTextArea("",10,45);
         textArea.setEditable(false);
         textArea.setFont(serif);
         pane = new JScrollPane(textArea);
@@ -43,12 +45,21 @@ public class FortuneTellerFrame extends JFrame{
         sans = new Font(Font.SANS_SERIF, Font.BOLD, 18);
         readBtn = new JButton("Read my fortune!");
         readBtn.setFont(sans);
-        readBtn.addActionListener((ActionEvent ae) ->
-        {});
+        readBtn.addActionListener((ActionEvent ae) -> {
+            if(i==12){ //first time through just pick a random fortune
+                i = (short)(Math.random()*12);
+                textArea.append(fortunes[i]);
+            }
+            else { //pick a fortune 0-10 and if it's the same or higher than the last one, add 1
+                short j = (short)(Math.random()*11);
+                if(j>=i){j++;}
+                i=j;
+                textArea.append("\n"+fortunes[i]);
+            }
+        });
         quitBtn = new JButton("Quit");
         quitBtn.setFont(sans);
-        quitBtn.addActionListener((ActionEvent ae) ->
-        {});
+        quitBtn.addActionListener((ActionEvent ae) -> {System.exit(0);});
         bottom = new JPanel();
         bottom.add(readBtn);
         bottom.add(quitBtn);
@@ -57,7 +68,7 @@ public class FortuneTellerFrame extends JFrame{
         add(mainPnl);
 
         setTitle("Fortune Teller");
-        setSize(800, 600);
+        setSize(800, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
