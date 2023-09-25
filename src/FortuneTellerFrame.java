69e9d6b3115d09cc0002de29696e5c18418e9cd3
dominoPC <89866615+dominoPC@users.noreply.github.com>
2023-09-25 10:56:18 -0400

    layout

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -1,9 +1,65 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 public class FortuneTellerFrame extends JFrame{
+    JPanel mainPnl;
+    ImageIcon ball;
+    JLabel imageLbl;
+    Font mono;
+    JScrollPane pane;
+    JTextArea textArea;
+    JPanel top;
+    JPanel middle;
+    JPanel bottom;
+    Font serif;
+    Font sans;
+    JButton readBtn;
+    JButton quitBtn;
+
     public FortuneTellerFrame() {
+
+        mainPnl = new JPanel();
+        mainPnl.setLayout(new GridLayout(3,1));
+        ball = new ImageIcon(".\\src\\ball.jpg");
+        imageLbl = new JLabel("Fortune Telling",ball,SwingConstants.CENTER);
+        mono = new Font(Font.MONOSPACED, Font.PLAIN, 36);
+        imageLbl.setFont(mono);
+        top = new JPanel();
+        top.add(imageLbl);
+        mainPnl.add(top);
+
+
+        serif = new Font(Font.SERIF, Font.PLAIN, 18);
+        textArea = new JTextArea("fortunes",10,40);
+        textArea.setEditable(false);
+        textArea.setFont(serif);
+        pane = new JScrollPane(textArea);
+        middle = new JPanel();
+        middle.add(pane);
+        mainPnl.add(middle);
+
+        sans = new Font(Font.SANS_SERIF, Font.BOLD, 18);
+        readBtn = new JButton("Read my fortune!");
+        readBtn.setFont(sans);
+        readBtn.addActionListener((ActionEvent ae) ->
+        {});
+        quitBtn = new JButton("Quit");
+        quitBtn.setFont(sans);
+        quitBtn.addActionListener((ActionEvent ae) ->
+        {});
+        bottom = new JPanel();
+        bottom.add(readBtn);
+        bottom.add(quitBtn);
+        mainPnl.add(bottom);
+
+        add(mainPnl);
+
         setTitle("Fortune Teller");
-        setSize(500, 400);
+        setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
+
 }
--- src/FortuneTellerViewer.java
+++ src/FortuneTellerViewer.java
@@ -1,5 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class FortuneTellerViewer {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new FortuneTellerFrame();
     }
 }
